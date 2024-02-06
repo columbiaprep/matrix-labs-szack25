3739daca1db2a1815f109f2dc7d6e606a911448c
Sam Zack <samueltzack@gmail.com>
2024-02-06 12:31:23 -0500

    Tuesday work

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    int[][] scaleNumbers(int[][] array, int scale) {
+        // Create a new 2d array called output w/ row and col count same as input
+        int[][] output = new int[array.length][array[0].length];
+        // Iterate through the 2d array input
+
+        // For each value, multiply it by the scaling factor
+        // Place that value in the output array in the same spot
+    }
+}
