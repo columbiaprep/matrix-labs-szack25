07d03757ff527054e7285d071ef519a4a14eeeae
Sam Zack <samueltzack@gmail.com>
2024-02-08 14:09:58 -0500

    thursday work

--- Main.java
+++ Main.java
@@ -7,8 +7,16 @@ public class Main {
         // Create a new 2d array called output w/ row and col count same as input
         int[][] output = new int[array.length][array[0].length];
         // Iterate through the 2d array input
+        for (int r = 0; r < output.length; r++) {
+            for (int c = 0; c < output[r].length; c++) {
+                // For each value, multiply it by the scaling factor
+                // Place that value in the output array in the same spot
+                output[r][c] *= scale;
+            }
+        }
+        return output;
+    }
+    int multiplyMatrices(int[][] matrix1, int[][] matrix2) {
 
-        // For each value, multiply it by the scaling factor
-        // Place that value in the output array in the same spot
     }
 }
